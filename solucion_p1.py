c13453bd9df545dc76b2590d8956cbc66a19a5b3
Sergio <sergio.munoz@upm.es>
2021-10-06 12:08:02 +0200

    updated README and solucion_p1

---
+++ solucion_p1.py
@@ -0,0 +1,14 @@
+EURO_BITCOIN_RATE = 44471.78
+
+def sumar_numeros(num1, num2):
+    '''Suma los dos numeros proporcionados.'''
+    suma = num1 + num2
+    return suma
+
+def euros_a_bitcoins(euros):
+  '''Convierte una cantidad de euros a bitcoins. 1 bitcoin = 44570.17 €'''
+  raise NotImplementedError
+
+def bitcoins_a_euros(usd):
+  '''Convierte una cantidad de bitcoins a euros. 1 bitcoin = 44570.17 €'''
+  raise NotImplementedError
